c51db457c0d4259db1cfc1dafd56540e8f489e86
daydreams <zn.moon2016@gmail.com>
2017-12-10 17:29:54 +0800

    update

--- ip2long/install.sh
+++ ip2long/install.sh
@@ -1,4 +1,4 @@
-g++ -std=c++11 long2ip.cpp -o long2ip
-g++ -std=c++11 ip2long.cpp -o ip2long
+g++ -std=c++11 ./src/long2ip.cpp -o long2ip
+g++ -std=c++11 ./src/ip2long.cpp -o ip2long
 mv long2ip /usr/local/bin/
 mv ip2long /usr/local/bin/
